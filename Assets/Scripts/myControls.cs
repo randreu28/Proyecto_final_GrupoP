c00f7d7478d6c93b6096883543557c95522b2664
Rubén Chiquin <randreu28@gmail.com>
2022-04-27 11:13:25 +0200

    Lobby colliders & finished 1st person player controller

--- Assets/Scripts/myControls.cs
+++ Assets/Scripts/myControls.cs
@@ -13,8 +13,10 @@ public class myControls : MonoBehaviour
 
     [Range(0, 1)]
     public float sprintSpeed;
+    private bool isSprinting = false;
 
-    private Vector3 movementVec;
+    private Vector3 walkVec;
+    private Vector3 sprintVec;
     private Quaternion rotationCam;
 
     void Start()
@@ -27,14 +29,35 @@ public class myControls : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _controller.Move(movementVec);
-        rotationCam = cam.transform.rotation;
+        rotationCam = cam.transform.rotation; //For making the player move on the direction it's facing
+        rotationCam.eulerAngles = new Vector3(0, rotationCam.eulerAngles.y, rotationCam.eulerAngles.z); //Making it so it doesnt affect the z axis
+        if (isSprinting)
+        {
+            _controller.Move((rotationCam * sprintVec));
+        }
+        else
+        {
+            _controller.Move((rotationCam * walkVec));
+        }
+
     }
 
     void OnMove(InputValue input)
     {
         Vector2 moveInput = input.Get<Vector2>();
-        moveInput =  rotationCam * moveInput; //For making the player move on the direction it's facing
-        movementVec = new Vector3(moveInput.x * walkSpeed, 0, moveInput.y * walkSpeed);
+        walkVec = new Vector3(moveInput.x * walkSpeed, 0, moveInput.y * walkSpeed);
+        sprintVec = new Vector3(moveInput.x * sprintSpeed, 0, moveInput.y * sprintSpeed);
+    }
+
+    void OnSprint(InputValue input)
+    {
+        if (input.isPressed)
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
     }
 }
