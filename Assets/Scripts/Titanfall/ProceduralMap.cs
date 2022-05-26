42018ebd1baa43934696ed40b7491a905b5ae557
Rubén Chiquin <randreu28@gmail.com>
2022-05-26 10:56:48 +0200

    Actualiza ProceduralMap
    Ahora existen variables publicas para controlar el randomizado de las rotaciones

--- Assets/Scripts/Titanfall/ProceduralMap.cs
+++ Assets/Scripts/Titanfall/ProceduralMap.cs
@@ -6,12 +6,20 @@ public class ProceduralMap : MonoBehaviour
 {
     public GameObject[] Prefabs;
 
+    public bool rotatesX = false;
+    public bool rotatesY = false;
+    public bool rotatesZ = false;
+
     void Start()
     {
+        float _rotationX = rotatesX ? 1*(Random.Range(0,4)*45) : 1;
+        float _rotationY = rotatesY ? 1*(Random.Range(0,4)*45) : 1;
+        float _rotationZ = rotatesZ ? 1*(Random.Range(0,4)*45) : 1;
+
         Instantiate(
             Prefabs[Random.Range(0, Prefabs.Length)],
             transform.position,
-            Quaternion.Euler(Vector3.up*(Random.Range(0,4)*45))
+            Quaternion.Euler(new Vector3(_rotationX, _rotationY, _rotationZ))
         );
     }
 }
